425cbf1ab494c5fad1f9e89b4b8e6bc1ff038757
Konrad Markus <konker@luxvelocitas.com>
2016-06-24 17:12:05 +0300

    Cosmetic amend to build.sh output

--- build.sh
+++ build.sh
@@ -2,3 +2,6 @@
 
 rm -f TuomiokirkonKello.zip
 zip -r TuomiokirkonKello.zip index.js bongi-bot.js package.json config.json node_modules
+
+echo "----------------------------"
+echo "Created TuomiokirkonKello.zip"
